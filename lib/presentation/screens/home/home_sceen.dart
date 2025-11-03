a680c2573d109244a60a4956786411901fb99667
Tu nombre <julioandres171104@gmail.com>
2025-11-03 00:08:33 -0600

    se agrego el home

---
+++ lib/presentation/screens/home/home_sceen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeSceen extends StatelessWidget {
+  const HomeSceen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Home Screen'),
+      ),
+    );
+  }
+
+}
